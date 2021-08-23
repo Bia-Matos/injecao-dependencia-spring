87732cc483dadb1dc84ca50ce893cd9204af99e9
Beatriz <beatriz.matos@dtidigital.com.br>
2021-08-23 11:48:12 -0300

    Configurações iniciais

---
+++ src/main/java/com/beatriz/injecaodependenciaspring/InjecaoDependenciaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.beatriz.injecaodependenciaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InjecaoDependenciaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InjecaoDependenciaSpringApplication.class, args);
+    }
+
+}
